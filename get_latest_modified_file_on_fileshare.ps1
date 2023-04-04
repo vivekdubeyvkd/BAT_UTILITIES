19a4e2189d96fbef0ffcc1cbd3b469df61456199
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2023-04-04 20:56:24 +0530

    adding code to find files changed in last few mins along with logic to find files changed in last few hours

--- get_latest_modified_file_on_fileshare.ps1
+++ get_latest_modified_file_on_fileshare.ps1
@@ -1,3 +1,4 @@
+## to find files updated last few hours
 # define threshold in hours to find files modified or updated in parent folder 
 $modifiedFileThreshholdInHour=24
 
@@ -9,6 +10,25 @@ Get-ChildItem -Path "$fileParentPath" -Recurse -File | Foreach {
     $lastUpdateFileTime=$_.LastWriteTime
     $nowTime = get-date
     if (($nowTime - $lastUpdateFileTime).totalhours -le $modifiedFileThreshholdInHour)
+    {
+    	$inputFilePath=Join-Path $_.Directory  $_.Name
+        # print file changed timestamp, file name and absolute file path 
+        Write-Host $_.LastWriteTime, $_.Name, $inputFilePath
+    }
+}
+
+## to find files updated last few minutes
+# define threshold in minutes to find files modified or updated in parent folder 
+$modifiedFileThreshholdInMinute=1440
+
+# define parent folder 
+$fileParentPath="C:\Program Files"
+
+# find files recursively to list the updated files within defined threshold under parent folder 
+Get-ChildItem -Path "$fileParentPath" -Recurse -File | Foreach {
+    $lastUpdateFileTime=$_.LastWriteTime
+    $nowTime = get-date
+    if (($nowTime - $lastUpdateFileTime).totalminutes -le $modifiedFileThreshholdInMinute)
     {
     	$inputFilePath=Join-Path $_.Directory  $_.Name
         # print file changed timestamp, file name and absolute file path 
